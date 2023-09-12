03a6d86402a7f684a93a161177cf2eb814565434
varundhand <varundhand77@gmail.com>
2023-09-12 22:42:24 +0530

    Feat: Emojis Feature

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,32 +1,61 @@
 import Link from "next/link"
+import { useState } from "react"
+
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import {BsEmojiSmile} from 'react-icons/bs'
 
 const Form = ({type, post, setPost, submitting, handleSubmit}) => {
+  const [showEmojis, setshowEmojis] = useState(false);
+
+  const addEmoji = (e) => {
+    const sym = e.unified.split("_");
+    const codeArray = [];
+    sym.forEach((element) => codeArray.push("0x" + element));
+    let emoji = String.fromCodePoint(...codeArray)
+    setPost(prevPost => ({...prevPost, description: post.description + emoji}))
+  }
+
   return (
 
-    <section className="w-full max-w-full felx-start flex-col">
-      <h1 className="head_text text-left">
+    <section className='w-full max-w-full flex flex-col items-center'>
+      
+      
+      <h1 className="head_text text-center">
         <span className="orange_gradient text "> {type} Post</span>
       </h1>
-      <p className="desc text-left max-w-md">
+      <p className="desc text-center max-w-md">
         What's on your mind about coding today?
       </p>
 
       <form 
         onSubmit={handleSubmit}
-        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        className="mt-10 mb-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
         <label>
           <span className='font-satoshi font-semibold text-base text-black'>
             Your Post's Description
           </span>
+        
+          <div className="flex relative">
+            <textarea
+              value={post.description}
+              onChange={(e) => setPost({...post, description: e.target.value})}
+              placeholder="Write Your Post Here"
+              required
+              className="form_textarea "
+            />
+            <span 
+              className='mt-2 absolute top-2 right-2 opacity-40 hover:opacity-100'
+              onClick={() => setshowEmojis(!showEmojis)}
+            >
+              <BsEmojiSmile/>
+            </span>
+            <div className="absolute top-[20%] opacity-80 right-1 z-10">
+              {showEmojis && <Picker data={data} onEmojiSelect={addEmoji} /> }
+            </div>
+          </div>
 
-          <textarea
-            value={post.description}
-            onChange={(e) => setPost({...post, description: e.target.value})}
-            placeholder="Write Your Post Here"
-            required
-            className="form_textarea "
-          />
         </label>
 
         <label>
@@ -59,7 +88,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
             className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400"
             disabled={submitting}  
           >
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               {submitting ? `${type}...` : type }
             </span>
           </button>
