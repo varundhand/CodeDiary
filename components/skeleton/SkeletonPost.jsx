cc5d14e6c8deb84fffca91b07a204fbe10a0c678
varundhand <varundhand77@gmail.com>
2023-09-22 19:03:48 +0530

    Refactor: Skeleton Loading Responsive

--- components/skeleton/SkeletonPost.jsx
+++ components/skeleton/SkeletonPost.jsx
@@ -2,15 +2,13 @@ import Skeleton from './Skeleton'
 
 const SkeletonPost = () => {
     return (
-        <div className="post">
-          <div className="flex gap-3">
-            <Skeleton classes="title width-50" />
-            <Skeleton classes="text width-25" />
-          </div>
-            <Skeleton classes="text width-100" />
-            <Skeleton classes="text width-100" />
-            <Skeleton classes="text width-100" />
-        </div>
+      <div className="post">
+        <Skeleton classes="title width-30" />
+        <Skeleton classes="title width-50" />
+        <Skeleton classes="text width-100" />
+        <Skeleton classes="text width-100" />
+        <Skeleton classes="text width-100" />
+      </div>
     )
 }
 export default SkeletonPost
